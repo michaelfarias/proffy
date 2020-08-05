f35040bc2f577596cb5f30f1fd0535ce8df17ce0
michaelfarias <michaelfarias01@gmail.com>
2020-08-05 08:59:20 -0300

    backend finalizado

---
+++ server/src/routes.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
+
+const routes = express.Router();
+const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
+
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
+
+export default routes;
+
+// GET: Buscar ou listar uma informação
+// POST: Criar alguma nova informação
+// PUT: Atualizar uma informação existente
+// DELETE: Deletar uma informação existente
+
+// Corpo (Request Body): Dados para criação ou atualização de um registro
+// Route Params: Identificar qual recurso eu quero atualizar ou deletar
+// Query Params: Paginação, filtros, ordenação
